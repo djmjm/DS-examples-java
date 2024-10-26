99fb8cded7638f09d6ad03e650b46afdfeee2377
djmjm <douglasjanovikmaliszewski@gmail.com>
2024-10-26 19:05:50 -0300

    feat: add main class (stack)

---
+++ ds/src/main/java/br/com/ds/stack/main.java
@@ -0,0 +1,29 @@
+package br.com.ds.stack;
+
+public class main {
+    public static void main(String[] args) {
+        Stack stack = new Stack(10);
+
+        System.out.println(stack.print());
+        
+        System.out.println("--------------------");
+
+        for (int i = 0; i < 1000; i++) {
+            stack.push((int) (Math.random() * 100));
+        }
+
+       System.out.println(stack.print());
+
+       System.out.println("--------------------");
+
+       stack.pop(); stack.pop();
+
+       System.out.println(stack.print());
+
+       System.out.println("--------------------");
+
+       stack.push(1000);
+
+       System.out.println(stack.print());
+    }
+}

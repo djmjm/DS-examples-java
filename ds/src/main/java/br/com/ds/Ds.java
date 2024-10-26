3be59dbc6ec442d971cea4b72c7091d9ad7ad200
djmjm <douglasjanovikmaliszewski@gmail.com>
2024-10-26 09:09:05 -0300

    feat: add Ds.java class

---
+++ ds/src/main/java/br/com/ds/Ds.java
@@ -0,0 +1,12 @@
+package br.com.ds;
+
+/**
+ *
+ * @author Douglas Janovik Maliszewski
+ */
+public class Ds {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}

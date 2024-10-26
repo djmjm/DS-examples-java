d71d97bcd941e590fb8e0f5c41aba9703a70262e
djmjm <douglasjanovikmaliszewski@gmail.com>
2024-10-26 19:55:43 -0300

    feat(stack): add Tower of Hanoi example using stack methods

---
+++ ds/src/main/java/br/com/ds/stack/examples/TowerOfHanoi.java
@@ -0,0 +1,34 @@
+package br.com.ds.stack.examples;
+
+import br.com.ds.stack.Stack;
+
+/** ... 
+ * 
+ *  quebra-cabeças (inspirado no Torres de
+    Hanoi) em que existem 3 pilhas, sendo que duas delas são totalmente preenchidas
+    com números aleatórios e a terceira está totalmente vazia.
+    O objetivo é, manualmente, através de operações de pop-push, separe-se a metade
+    dos números menores em uma das pilhas e a metade dos maiores fique em outra
+    pilha.
+ * 
+ * 
+ * ----------------------------------------------------------------------------------
+ * 
+ * Puzzle (inspired by the Tower of Hanoi) with 3 stacks, 
+ * where two of them are completely filled with random numbers, 
+ * and the third is entirely empty. The goal is, manually, 
+ * through pop-push operations, to separate half of the smaller numbers 
+ * into one stack and half of the larger numbers into another stack.
+*/
+
+public class TowerOfHanoi {
+    private Stack stack1;
+    private Stack stack2;
+    private Stack stack3;
+
+
+    public TowerOfHanoi(int size){
+        
+    }
+
+}

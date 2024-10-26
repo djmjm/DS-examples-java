3053c34c53d48a1df181ae8439ac53e4323da283
djmjm <douglasjanovikmaliszewski@gmail.com>
2024-10-26 19:18:53 -0300

    feat: add new test case (stack)

--- ds/src/test/java/br/com/ds/StackTest.java
+++ ds/src/test/java/br/com/ds/StackTest.java
@@ -1,5 +1,6 @@
 package br.com.ds;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.BeforeEach;
@@ -62,6 +63,26 @@ public class StackTest {
 
         assertFalse(stack.push( (int) Math.random() ));
         assertTrue(stack.isFull());
+       
+    }
+
+    @Test
+    void testReturnValue(){
+        assertTrue(stack.push( 1));
+        assertTrue(stack.push( 2 ));
+        assertTrue(stack.push( 3 ));
+
+        assertTrue(stack.pop());
+        assertEquals(3, stack.getValueTop());
+
+        assertTrue(stack.pop());
+        assertEquals(2, stack.getValueTop());
+
+        assertTrue(stack.pop());
+        assertEquals(1, stack.getValueTop());
+
+        assertFalse(stack.pop());
+        assertTrue(stack.isEmpty());
        
     }
 

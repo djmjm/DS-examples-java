d0e1c516bcd5331d7837d6f69fd61e8ebf7c3b10
djmjm <douglasjanovikmaliszewski@gmail.com>
2024-10-27 17:31:32 -0300

    feat(TowerOfHanoi): add move method

--- ds/src/main/java/br/com/ds/stack/examples/TowerOfHanoi.java
+++ ds/src/main/java/br/com/ds/stack/examples/TowerOfHanoi.java
@@ -1,6 +1,7 @@
 package br.com.ds.stack.examples;
 
 import br.com.ds.stack.Stack;
+import br.com.ds.stack.examples.TowerOfHanoi.Movements;
 
 /** ... 
  * 
@@ -21,14 +22,75 @@ import br.com.ds.stack.Stack;
  * into one stack and half of the larger numbers into another stack.
 */
 
+
 public class TowerOfHanoi {
+
+    public enum Movements {
+        STACK1, STACK2, STACK3
+    };
+
     private Stack stack1;
     private Stack stack2;
     private Stack stack3;
 
 
     public TowerOfHanoi(int size){
-        
+
+        this.stack1 = new Stack(size, true);
+        this.stack2 = new Stack(size, true);
+        this.stack3 = new Stack(size, true);
+
+        for(int i = 0; i < size; i++){
+            stack1.push( (int) Math.random() * size + 1  );
+            stack2.push( (int) Math.random() * size + 1  );
+        }
     }
+
+    public boolean move( Movements from, Movements to ){
+
+        if(from == to){
+            return false;
+        }
+
+        if(from == Movements.STACK1){
+            if(to == Movements.STACK2) { 
+                this.stack1.pop();
+                return this.stack2.push(  this.stack1.getValueTop()  );
+             }
+
+             if(to == Movements.STACK3) { 
+                this.stack1.pop();
+                return this.stack3.push(  this.stack1.getValueTop()  );
+             }
+        }
+
+        if(from == Movements.STACK2){
+            if(to == Movements.STACK1) { 
+                this.stack2.pop();
+                return this.stack1.push(  this.stack2.getValueTop()  );
+             }
+
+             if(to == Movements.STACK3) { 
+                this.stack2.pop();
+                return this.stack3.push(  this.stack2.getValueTop()  );
+             }
+        }
+
+        if(from == Movements.STACK3){
+            if(to == Movements.STACK1) { 
+                this.stack3.pop();
+                return this.stack1.push(  this.stack3.getValueTop()  );
+             }
+
+             if(to == Movements.STACK2) { 
+                this.stack3.pop();
+                return this.stack2.push(  this.stack3.getValueTop()  );
+             }
+        }
+
+
+        return true;
+    }
+
 
 }

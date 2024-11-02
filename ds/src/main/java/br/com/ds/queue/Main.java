9333aebb85b4207dd0904a746a561cf44402e8ed
djmjm <douglasjmjm@gmail.com>
2024-11-02 11:10:04 -0300

    feat(Queue/Main): add examples of Queue operations in Main class.

--- ds/src/main/java/br/com/ds/queue/Main.java
+++ ds/src/main/java/br/com/ds/queue/Main.java
@@ -3,6 +3,8 @@ package br.com.ds.queue;
 public class Main {
     public static void main(String[] args){
         Queue queue = new Queue(5);
+        Queue queue2 = new Queue(5);
+
         System.out.println(
             queue.print()
         );
@@ -13,15 +15,65 @@ public class Main {
         queue.enqueue(5);
         queue.enqueue(9);
         queue.enqueue(1);
+        queue.enqueue(1);
+        queue.enqueue(1);
+        queue.enqueue(1);
+        queue.enqueue(1);
+
+        System.out.println(queue.print());
+
+        System.out.println(
+            queue.peek()
+        );
+
+        queue.dequeue();
+
+        System.out.println(
+            queue.peek()
+        );
+
+        queue.dequeue();
+
+        System.out.println(
+            queue.peek()
+        );
+
+        queue.dequeue();
+
+        System.out.println(
+            queue.peek()
+        );
+
+        queue.dequeue();
+
+        System.out.println(
+            queue.peek()
+        );
+
+        queue.dequeue();
+
+        System.out.println(
+            queue.peek()
+        );
+
+        queue.enqueue(999);
 
         System.out.println(
             queue.print()
         );
+
+        queue.enqueue(-2);
+
+        System.out.println(
+            queue.print()
+        );
+
         System.out.println(
             queue.peek()
         );
-        System.out.println(
-            queue.getSize()
-        );
+
+        
+
+
     }
 }

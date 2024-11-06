76bdcb4b42321c8fe3f16b0688b8dc37e0472813
djmjm <douglasjanovikmaliszewski@gmail.com>
2024-11-06 16:23:11 -0300

    feat(list/LinkedList): add push and pop methods.

--- ds/src/main/java/br/com/ds/list/LinkedList.java
+++ ds/src/main/java/br/com/ds/list/LinkedList.java
@@ -1,5 +1,28 @@
 package br.com.ds.list;
 
 public class LinkedList {
-    
+    private Element start;
+
+    public boolean push(Element elem){
+        try{
+            elem.setNext(start);
+            start = elem;
+            return true;
+        }catch(Exception e){
+            return false;
+        }
+    }
+
+    public Element pop(){
+        if(start == null){
+            return null;
+        }
+
+        Element elem = start;
+        start = elem.getNext();
+        elem.setNext(null);
+
+        return elem;
+    }
+
 }

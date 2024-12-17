2364b752f88c619634bf1eaa5ea125564bfdbcf8
djmjm <douglasjanovikmaliszewski@gmail.com>
2024-12-17 14:40:39 -0300

    feat(linkedlist): add find method with clockwise and counter-clockwise search to CircularDoublyLinkedList.

--- ds/src/main/java/br/com/ds/list/CircularDoublyLinkedList.java
+++ ds/src/main/java/br/com/ds/list/CircularDoublyLinkedList.java
@@ -4,4 +4,34 @@ public class CircularDoublyLinkedList {
     private ElementBack start;
     private ElementBack end;
     private int size;
+
+    boolean findClockwise(String id){
+        ElementBack elemIter = start;
+
+        do{
+            if(id == elemIter.getId()){
+                return true;
+            }
+            elemIter = (ElementBack) elemIter.getNext();
+        }while(elemIter != start);
+
+
+        return false;
+    }
+
+    boolean findCounterClockwise(String id){
+        ElementBack elemIter = start;
+
+        do{
+            if(id == elemIter.getId()){
+                return true;
+            }
+            elemIter = (ElementBack) elemIter.getBack();
+        }while(elemIter.getNext() != start);
+
+
+        return false;
+    }
+
+    
 }

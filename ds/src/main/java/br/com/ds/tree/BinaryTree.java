9c40213ab1e206658eb01c3ab1015efbc3557fe0
djmjm <douglasjanovikmaliszewski@gmail.com>
2025-01-04 09:24:09 -0300

    feat(tree): create BinaryTree class.

---
+++ ds/src/main/java/br/com/ds/tree/BinaryTree.java
@@ -0,0 +1,11 @@
+package br.com.ds.tree;
+
+import br.com.ds.util.UniqueRandom;
+
+public class BinaryTree {
+    private UniqueRandom idGenerator;
+    
+    public BinaryTree(){
+        idGenerator = new UniqueRandom(10);
+    }
+}

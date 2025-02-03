521f28d5bbd46d513aef2c5f52566afa5c41af75
djmjm <douglasjanovikmaliszewski@gmail.com>
2025-02-03 10:10:12 -0300

    feat(avltree): add rotation methods to AVLTree.

--- ds/src/main/java/br/com/ds/tree/AVLTree.java
+++ ds/src/main/java/br/com/ds/tree/AVLTree.java
@@ -1,5 +1,53 @@
 package br.com.ds.tree;
 
 public class AVLTree extends BinaryTree{
-    
+    public AVLTree(){
+        super();
+    }
+
+    int height(Node node) {
+        return (node == null) ? 0 : node.getHeight();
+    }
+
+    void updateHeight(Node node) {
+        if (node != null) {
+            node.setHeight(1 + Math.max(height(node.getChild_left()), height(node.getChild_right())) );
+        }
+    }
+
+    Node rotateRight(Node y) {
+        Node x = y.getChild_left();
+        Node T2 = x.getChild_right();
+
+        x.setChild_right(y);
+        y.setChild_left(T2);
+
+        updateHeight(y);
+        updateHeight(x);
+
+        return x;
+    }
+
+    Node rotateLeft(Node x) {
+        Node y = x.getChild_right();
+        Node T2 = y.getChild_left();
+
+        y.setChild_left(x);
+        x.setChild_right(T2);
+
+        updateHeight(x);
+        updateHeight(y);
+
+        return y;
+    }
+
+    Node rotateLeftRight(Node node) {
+        node.setChild_left(rotateLeft(node.getChild_left()));
+        return rotateRight(node);
+    }
+
+    Node rotateRightLeft(Node node) {
+        node.setChild_right(rotateRight(node.getChild_right()));
+        return rotateLeft(node);
+    }
 }
